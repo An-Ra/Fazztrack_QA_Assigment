87d447d49e23adc8087924345d22fa3fc02d8413
andy.mahendra <andymahendra80@gmail.com>
2022-09-01 10:09:48 +0700

    Assignment_5

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://b899-13-67-75-93.ngrok.io',
+    baseUrl: 'http://bcb7-13-67-75-93.ngrok.io',
   },
 });
